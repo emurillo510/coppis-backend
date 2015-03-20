2e306aca1c38672fdc92ec1c75a9ea45fc842536
Elton Murillo <Elton Murillo>
2015-03-19 18:10:17 -0700

    Adds brands

--- config/routes.rb
+++ config/routes.rb
@@ -56,10 +56,12 @@ Rails.application.routes.draw do
 
       namespace :api do
         namespace :v1 do
-
+           
           resources :posts do
 	    resources :comments
 	  end
+
+	  resources :brands
 
 	end
       end
