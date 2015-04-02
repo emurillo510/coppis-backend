a948f70c9cac4645e8e59eb0516b748180588f3f
Elton Murillo <emurillo510@gmail.com>
2015-04-01 20:44:12 -0700

    Templates for controller tests

--- spec/controllers/api/v1/brands_controller_spec.rb
+++ spec/controllers/api/v1/brands_controller_spec.rb
@@ -2,4 +2,20 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::BrandsController, type: :controller do
 
+#set-up
+
+#context1 index
+ 
+#context2 new 
+
+#context3 create
+
+#context4 show
+
+#context5 edit
+ 
+#context6 update
+
+#context7 destroy
+
 end
